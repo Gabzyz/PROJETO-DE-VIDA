94598114c1fcfd6fb21287e618fecb8998c8fe41
Gabzyz <alves.santos.gabriel2511@escola.pr.gov.br>
2026-05-18 15:17:27 +0000

    oi

--- script.js/script.js
+++ script.js/script.js
@@ -1,7 +1,19 @@
 const botoes = document.querySelectorAll(".botao");
+const botoes = document.querySelectorAll(".aba-conteudo");
 for (let i =0; 1 < botoes.length; i++){
-    botoes[i].onclick = function {
-        botoes[1].classList(".ativo")
-    }
+    botoes[i].onclick = function (){
+        for (let j=0; j<botoes.length; j++){
+        botoes[i].classList.remove("ativo")
+        testos[j].classlist.remove("ativo")
+        }
+    botoes[i].classList.add("ativo")
+    textos[i].classList.add("ativo")
+    } 
 console.log(botoes);
 }
+const contadores = document.querySelectorAll("contador");
+const tempoObjetivol = new Date("2026-12-28T23:59:59");
+const tempoObjetivol = new Date("2026-12-28T23:59:59");
+const tempoObjetivol = new Date("2026-12-28T23:59:59");
+const tempoObjetivol = new Date("2026-12-28T23:59:59");
+const tempoObjetivol = new Date("2026-12-28T23:59:59");
